25db6152bff9621d311017b242771f5325e319e9
Your Nameecho <you@example.com>
2024-05-20 12:23:16 +0530

    modified print added

--- index.py
+++ index.py
@@ -3,9 +3,12 @@ from lib.arrum import FindLarge, FindSmall
 def Test() :
     data = []
     limit = int(input("Enter limit\n"))
+    print("--------------------------------")
     for i in range(limit) :
         listData = int(input("Enter Your Data\n"))
         data.append(listData)
+    print("Your List :", data)
+    print("--------------------------------")
     print("Large number is :",FindLarge(data))
     print("Small number is :",FindSmall(data))
 Test()
